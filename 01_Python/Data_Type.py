33486fd85649462f696f53674f845ade8f4f109d
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-03 15:43:31 +0000

    Data types

---
+++ 01_Python/Data_Type.py
@@ -0,0 +1,15 @@
+# int,Float,Boolean,string etc 
+
+a = int(input())
+b = a * 45
+print(b)
+
+a = float(input()) # int can be in input if float is used 
+b =  a / 43
+print(b)
+
+#Boolean , string
+a = input()
+b = (a == "shreya")
+print(b)
+
