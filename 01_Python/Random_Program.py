923f684e626cab221fe8261b2eed2a33f49f8ac4
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-03 16:08:21 +0000

    Different Operations

---
+++ 01_Python/Random_Program.py
@@ -0,0 +1,27 @@
+# Greater than 100
+a = int(input())
+b = int(input())
+c = a + b
+d = c > 100
+print(d)
+
+# Remider
+a = int(input())
+b = a % 2
+print(b)
+
+# Power 
+a = int(input())
+b = a ** 4
+print(b)
+
+# Divisor
+a = int(input()) 
+b = a // 3
+print(b)
+
+# not equal to 
+a = int(input())
+b = a != 54
+print(b)
+
