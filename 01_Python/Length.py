55994d810b585d7cd2ea6e01fe01ddf018d5a7fd
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-03 15:33:31 +0000

    Length of string

---
+++ 01_Python/Length.py
@@ -0,0 +1,30 @@
+full length
+a = input()
+b = len(a)
+print(b)
+
+first character
+a = input()
+b = a[0]
+print(b)
+
+second character
+a = input ()
+b = a[1]
+print(b)
+
+# Note: length always start from 1 but if we talk about particular character it start from 0
+a = input()
+b = a [:] 
+print(b)
+
+a = input()
+b = a[0:3] # last character +1
+print(b)
+
+first and last digit
+a = input()
+b = a[0]
+c = a[-1]
+print(b)
+print(c)
