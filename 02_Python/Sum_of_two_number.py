a5fb33edaeaeff7bac7526aae8c72c42bfa2541a
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-06 15:32:07 +0000

    Sum of two numbers

---
+++ 02_Python/Sum_of_two_number.py
@@ -0,0 +1,8 @@
+# write a program that reads two numbers and print the sum of two numbers in the given format.
+#Ex : a = 2.0 and b = 3.0
+# output- 5.0
+
+a = float(input())
+b = float(input())
+c = float(a + b)
+print(c)
