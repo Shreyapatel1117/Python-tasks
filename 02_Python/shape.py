e7541f44ee52c24f5ffcba25d7750b7355eaf077
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-09 17:22:54 +0000

    shape

---
+++ 02_Python/shape.py
@@ -0,0 +1,8 @@
+# write a program that reads a number N and prints three lines with each line containing N pluses (+).
+a = int(input())
+b = "+ "*a
+c = "+ "*a
+d = "+ "*a
+print(b)
+print(c)
+print(d)
