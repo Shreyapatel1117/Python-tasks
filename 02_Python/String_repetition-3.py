dbaad7f7a69e43257693452700d071f3b74fb496
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-09 15:34:59 +0000

    String Repetition-3

---
+++ 02_Python/String_repetition-3.py
@@ -0,0 +1,6 @@
+# given a word and a number(N), write a program to print the last three characters of the word N times in a single line.
+a = input()
+b = int(input())
+c = a[-3:]
+d = c *b
+print(d)
