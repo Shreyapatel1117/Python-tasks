4726c910322d89ef215bb3dca839057c625e562e
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-13 03:24:49 +0000

    Percentage 2

---
+++ 02_Python/Percentage-2.py
@@ -0,0 +1,7 @@
+# write a program that reads a number N. N is divided into two part X and Y .
+# X is 30 percent of Y
+#Y is the remaining percentage of N . print Y
+
+a = int(input())
+y = (70/100)*a
+print(y)
