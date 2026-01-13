c1206d6a36aa2e1790c662811f9efa8fd89340a9
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-13 03:21:00 +0000

    Basic Arithematic

---
+++ 02_Python/Basic_Arithmetic.py
@@ -0,0 +1,11 @@
+# Write a program to take two inputs (say A and B) and print the result of the following operations:
+# 1. Addintion 2. Substraction 3. Multiplication
+
+a = int(input())
+b = int(input())
+c = a +b
+d = a -b
+e = a * b
+print(c)
+print(d)
+print(e)
