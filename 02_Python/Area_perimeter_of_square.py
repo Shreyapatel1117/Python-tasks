3e91135e13bb02d299ecdfaf72740b5fb0585090
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-13 03:28:52 +0000

    Area and Perimeter of square

---
+++ 02_Python/Area_perimeter_of_square.py
@@ -0,0 +1,7 @@
+# you are given side of square as input.Write a program to finf the perimeter and area of the square.
+
+a = int(input())
+b = "Area of the square is: " + str(a*a)
+c = "Perimeter of the square is: " + str(4*a)
+print(b)
+print(c)
