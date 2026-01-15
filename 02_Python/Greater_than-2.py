7648702e37055bd3cd22827c3b2f38468cd0d9b0
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-15 16:11:06 +0000

    Greater among two number

---
+++ 02_Python/Greater_than-2.py
@@ -0,0 +1,6 @@
+# Write a program that reads two number A and B and checks if the A is greater than B. print the result as shown in the sample output.
+
+a = int(input())
+b = int(input())
+c = a > b
+print("A > B is " + str(c))
