8b2aa2714a6c84726fbfc0f2df23d5bd46045922
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-15 17:05:34 +0000

    Check if valid password or not

---
+++ 02_Python/Validate_Password.py
@@ -0,0 +1,5 @@
+# write a program to check ifthe given string is a valid password or not. A string is considered as valid password if the number of characters present is greater than 7.
+
+a = input()
+b = 7 < len(a)
+print(b)
