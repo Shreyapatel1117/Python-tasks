3c67bb230d92542de0e2e42f50c746c526a81838
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-16 03:42:57 +0000

    Percentage-3

---
+++ 03_Python/Percentage-3.py
@@ -0,0 +1,7 @@
+# write a program that reads a percentage p and a number N and checks if the P percentage of 500 is equal to the number N.
+
+a = int(input()) 
+b = int(input())
+c = (a/100)* 500
+d = (c == b)
+print(d)
