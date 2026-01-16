aacffd99b372865096f192b425c34916e8663deb
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-16 03:21:13 +0000

    Compare Digits

---
+++ 02_Python/Compare_digits.py
@@ -0,0 +1,12 @@
+# write a program that reads a two digit number N and checks,
+# if the number N is greater than 25.
+# if the first digit of N is greater than the second digit of N
+# print the result as shown in sample output
+
+a = input()
+b = int(a) > 25
+c = int(a[0])
+d = int([1])
+e = c > d
+print(b)
+print(d)
