2a553e219831ec867e5573523844cd8414749529
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-18 17:00:59 +0000

    Not Greater than 100

---
+++ 03_Python/Not_Greater_than_100.py
@@ -0,0 +1,7 @@
+# write a program that reads two numbers A and B and checks if the sum of A and B is not greater than 100.
+a = int(input())
+b = int(input())
+c = a +b
+d = c > 100
+e = not(d)
+print(e)
