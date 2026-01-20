e1e59adee25458c17a6055055dfafaeeebc1f819
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-20 02:52:56 +0000

    Compare Numbers-2

---
+++ 03_Python/Compare_numbers-2.py
@@ -0,0 +1,7 @@
+# write a program that reads that reads two numbers A and B , and 
+#checks if one of the given numbers is a negative number and the sum of the given number s greater than 7.
+
+a = int(input())
+b = int(input())
+c = (a < 0 or b < 0) and (a +b > 7)
+print(7)
