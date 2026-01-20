5ac046451de1c62c596d4d2d001d25a3c6d23981
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-20 03:22:56 +0000

    Entrance

--- 03_Python/Entrance.py
+++ 03_Python/Entrance.py
@@ -1,6 +1,6 @@
-# write a program that reads an age A and guardian status B, checks if the age A is between 12 and 60 or if the guardian sataus B is equal to yes.
+# write a program that reads an age A and guardian status S, checks if the age A is between 12 and 60 or if the guardian sataus S is equal to yes.
 
 a = int(input())
-b = input()
-c = (12 < a < 60) or (b == "yes")
+s = input()
+c = (12 < a < 60) or (s == "yes")
 print(c)
