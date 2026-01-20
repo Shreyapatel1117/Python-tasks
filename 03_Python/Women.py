3e9ce97754238fb66954a72e174d69e406ea5169
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-20 03:05:38 +0000

    Check if value is Woman

---
+++ 03_Python/Women.py
@@ -0,0 +1,7 @@
+# write a program that reads three strings A , B and C, and checks if one of the given strings is equal to woman.
+
+a = input()
+b = input()
+c = input()
+d = (a == "Woman") or (b == "Woman") or (c == "Woman")
+print(d)
