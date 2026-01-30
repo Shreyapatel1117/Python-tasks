ceb6f5d37f78ece9289541bcdb0dbdfb3f025b31
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-30 03:24:29 +0000

    Vote Eligible

---
+++ 03_Python/Vote_eligibility.py
@@ -0,0 +1,9 @@
+# write program that reads the age of a person and checks if the age of the person is greater than or equal to 18 for eligibility to vote.print
+# print Eligibility if the age of the person is greater than or equal to 28 . otherwise print Not Eligible.
+
+a = int(input())
+if a >= 18:
+    print("Eligible")
+else:
+    print("Not Eligible")
+     
