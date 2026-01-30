6dbe89d064699afdbe60c39b2928e6568f28b9c1
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-30 03:17:04 +0000

    Exam Result

---
+++ 03_Python/Exam_result.py
@@ -0,0 +1,9 @@
+# write a program that reads the student's marks as input and prints PASS or FAIL. if the student has scored more than 50,
+# print PASS. in all other cases print FAIL.
+
+a = int(input())
+if a > 50:
+    print("PASS")
+else:
+    print("FAIL")
+    
