7d6e19bd59efce199b1b85dcd192e5d014ae0beb
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-01-30 03:13:46 +0000

    Positive Integer

---
+++ 03_Python/Positive_integer.py
@@ -0,0 +1,10 @@
+# write a program that reads a number and convertsit to a positive number.
+# if the given number is negative,convert it to positive number and print it.ptherwise,print the given number
+
+a = int(input())
+b = a *(-1)
+if a <0 :
+    print(b)
+else:
+    print(a)
+    
