f898ff493e9e9a7d1a076b9784ded3a703ddcce6
Shreya Manavadariya <shreyamanavadariya@gmail.com>
2026-02-03 15:58:51 +0000

    Go for walk

---
+++ 03_Python/Go_for_walk.py
@@ -0,0 +1,7 @@
+# write a program that reads a temperature and checks if the given temperature is between 15 and 40.
+a = int(input())
+if 15< a < 40:
+    print("Can go for walk")
+else:
+    print("Cannot go for walk")
+    
